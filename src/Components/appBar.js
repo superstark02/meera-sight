26dc28dabb92e94f30c88e922a360f4c44c14cf2
superstark02 <superstark02@gmail.com>
2020-10-10 10:15:36 +0530

    10/10/2020

--- src/Components/appBar.js
+++ src/Components/appBar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../App.css'
+import logo from '../Images/Home/logo.png'
 
 function AppBar() {
   return (
     <React.Fragment>
-      
+      <div style={{marginTop:"10px"}} >
+        <img alt="logo" width="200px" src={logo} />
+      </div>
     </React.Fragment>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import AppBar from './Components/appBar'
 import * as serviceWorker from './serviceWorker';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppBar/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
